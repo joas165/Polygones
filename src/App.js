affdb8453682a3c36659f9dcd5b74417dbc9166a
joas165 <jaspegren@outlook.com>
2023-12-03 17:35:44 +0200

    removed some unused files

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 const App = () => {
   
-  const [SXCoordinates, updateSXCoordinates] = useState('');
-  const [sCoordinates, updateCoordinates] = useState([{x_coordinate:'', y_coordinate: ''}]);
+  const [SXCoordinates, updateSXCoordinates] = useState('0,0');
+  const [sCoordinates, updateCoordinates] = useState([{r_coordinate:'0', angle_coordinate: '0'}]);
 
   const handleFormChange = (index, event) => {
     let data = [...sCoordinates];
@@ -15,8 +15,8 @@ const App = () => {
  
   const increaseEdges = () => {
     let edge = {
-      x_coordinate: '',
-      y_coordinate: '',
+      x_coordinate: '0',
+      y_coordinate: '0',
     }
 
     updateCoordinates([...sCoordinates, edge]);
@@ -26,24 +26,41 @@ const App = () => {
     let data = [...sCoordinates];
     data.splice(index, 1)
     updateCoordinates(data)
-}
+  }
   
-  
+  const xCoordinate = (r,angle) => {
+    let nr = Number(r)
+    let nangle = Number(angle)
+    return String(nr*Math.acos(nangle*Math.PI/180));
+  }
+ 
+  const yCoordinate = (r,angle) => {
+    let nr = Number(r)
+    let nangle = Number(angle)
+    return String(nr*Math.asin(nangle*Math.PI/180));
+  }
+
+  //Canvas size 1000*1000
+  const translationToCenter = (coordinate) => {
+    let ncoordinate = Number(coordinate);
+    return String(500 + ncoordinate); 
+  }
+
   const submit = (event) => {
     event.preventDefault();
     let data = [...sCoordinates];
-    let outputX = data.map(element => element.x_coordinate);
-    let outputY = data.map(element => element.y_coordinate);
-    let output = [];
+    let outputX = data.map(element => translationToCenter(element.r_coordinate));
+    let outputY = data.map(element => translationToCenter(element.angle_coordinate));
+    let xyCoordinates = [];
     for (let i = 0; i < outputX.length;i++) { 
-      output.push(outputX[i]);
-      output.push(outputY[i]); 
+      xyCoordinates.push(xCoordinate(outputX[i],outputY[i]));
+      xyCoordinates.push(yCoordinate(outputX[i],outputY[i]));
     }
     updateCoordinates(sCoordinates);
-    updateSXCoordinates(output);
+    updateSXCoordinates(xyCoordinates);
     console.log(SXCoordinates);
     console.log(sCoordinates);  
-    console.log(output);  
+    console.log(xyCoordinates);  
   }
 
   
@@ -55,38 +72,31 @@ const App = () => {
           return (
        <div key={index}>  
             <input
-              name='x_coordinate'
-              placeholder='X-Coordinate'
-              value={form.x_coordinate}
+              name='r_coordinate'
+              placeholder='r-coordinate'
+              value={form.r_coordinate}
               onChange={event => handleFormChange(index,event)}
               />
             <input
-              name='y_coordinate'
-              placeholder='Y-Coordinate'
-              value={form.y_coordinate}
+              name='angle_coordinate'
+              placeholder='angle'
+              value={form.angle_coordinate}
               onChange={event => handleFormChange(index,event)} />
             
         </div>
         )})}
         </form>
-        <button
-        type="button"
-        onClick={submit}>
-        Draw</button>
-        <button onClick={increaseEdges}>Add Edges</button>
-        <button onClick={removeEdges}>Remove Edges</button>   
-      <svg width="500" height="500">
+        <div className="btn-group-vertical">
+          <button type="button" className="btn btn-primary btn-lg" onClick={submit}> Draw</button>
+          <button onClick={increaseEdges} className="btn btn-primary btn-lg"> Add Edges</button>
+          <button onClick={removeEdges} className="btn btn-primary btn-lg"> Remove Edges </button>   
+        </div>
+      <svg width="1000" height="1000">
        <polygon points={SXCoordinates} fill ="none" stroke = "red" />
       </svg>  
     </div>
     );
 }
-
-const Xcoordinates = [10,10,30,50,60,60];
-const Ycoordinates = [100,50,10,40,30,60]; 
-
-let SYCoordinates = Xcoordinates.map(String);
-let SXCoordinates = Ycoordinates.map(String); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
